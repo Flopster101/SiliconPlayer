5e5b8a986e387de3b8990454925d73f0bae29e5f
Flopster101 <nahuelgomez329@gmail.com>
2026-02-17 17:41:15 -0300

    gradle: Enable minify for release and optimized debug builds
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,8 @@ android {
             versionNameSuffix = "-debug"
         }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
         create("optimizedDebug") {
